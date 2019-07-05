6b0042b4d8b5f187cf39ff55e449044f558a5943
Goks <firehawk005@gmail.com>
2019-07-05 16:07:32 +0530

    initial Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
+
+</style>
